e9b8f2b59d994870eae0e50e37e812e62cfb8cd2
PeZet2 <zaremba.ph@gmail.com>
2021-09-09 21:20:45 +0200

    first steps...

--- keepass_manager.py
+++ keepass_manager.py
@@ -1,11 +1,35 @@
-import pykeepass
+#!/usr/bin/python3
+
+import sys
+from pykeepass import PyKeePass
+
+confpath = ""
+
+if len(sys.argv) > 1:
+    confpath = sys.argv[1]
+if confpath == "":
+    print("Error: No configuration path provided!")
+    exit(0)
 
 
+def readConf(filepath):
+    global dbpath, pass_enc
 
-class Keepass_Manager:
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            param = line.split("=")[0]
+            value = line.split("=")[1].rstrip()
+            if param == "DBPATH":
+                dbpath = value
+            elif param == "PASS_ENC":
+                pass_enc = value
+  
+readConf(confpath)
 
-    pass
+print(f"dbpath = {dbpath}")
+print(f"pass_enc = {pass_enc}")
 
 
+pass_dec = "pydb12"
 
-
+kp = PyKeePass(dbpath, password=pass_dec)
